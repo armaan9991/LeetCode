d1f98b005afe8c3089acd27f7698756335805714
armaan9991 <armaangill9991@gmail.com>
2026-06-20 23:24:29 -0600

    intialized dotnet

---
+++ LeetCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Contains_Duplicate_217 solution = new Contains_Duplicate_217();
+
+        int[] nums = { 1, 2, 3, 1 };
+
+        Console.WriteLine(solution.contain_Duplicate(nums));
+    }
+}
