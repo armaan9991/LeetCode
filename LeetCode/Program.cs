412e47b83eb1b99f9e16f9b13ba314741476d67d
armaan9991 <armaangill9991@gmail.com>
2026-06-20 23:39:34 -0600

    added some text cases for 217

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -8,7 +8,12 @@ class Program
         Contains_Duplicate_217 solution = new Contains_Duplicate_217();
 
         int[] nums = { 1, 2, 3, 1 };
+        int[] nums2 = { 1, 2, 3, 4 };
+        int[] nums3 = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
 
         Console.WriteLine(solution.contain_Duplicate(nums));
+        Console.WriteLine(solution.contain_Duplicate(nums2));
+        Console.WriteLine(solution.contain_Duplicate(nums3));
+
     }
 }
