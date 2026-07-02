8ac4b9e776c1a21a4833b8a8a12b038dde959b43
armaan9991 <armaangill9991@gmail.com>
2026-07-01 19:10:06 -0600

    MaxDepth of Binary Tree. 104

---
+++ LeetCode/BinaryTree/Maximum_Depth_104.cs
@@ -0,0 +1,22 @@
+ public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
+public class Maximum_Depth_104 {
+    public int MaxDepth(TreeNode root) {
+     if(root == null){
+        return  0;
+     }   
+     int left = MaxDepth(root.left);
+     int right  = MaxDepth(root.right);
+
+     return 1 + Math.Max(left,right);
+    }
+}
